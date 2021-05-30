976084bf8e3df0b5f6ec560e8ec40df3cac00d3b
Vidhyadharan23 <79758619+Vidhyadharan23@users.noreply.github.com>
2021-05-30 07:37:47 +0530

    Create 9.cpp

---
+++ WEEK6/9.cpp
@@ -0,0 +1,19 @@
+void printLevelOrder(node* root)
+{
+    int h = height(root);
+    int i;
+    for (i = 1; i <= h; i++)
+        printCurrentLevel(root, i);
+}
+void printCurrentLevel(node* root, int level)
+{
+    if (root == NULL)
+        return;
+    if (level == 1)
+        cout << root->data << " ";
+    else if (level > 1)
+    {
+        printCurrentLevel(root->left, level-1);
+        printCurrentLevel(root->right, level-1);
+    }
+}
