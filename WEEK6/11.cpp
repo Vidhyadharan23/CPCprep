98233fac918dfefd3a4057d078f96dbcb905d754
Vidhyadharan23 <79758619+Vidhyadharan23@users.noreply.github.com>
2021-05-30 22:46:12 +0530

    Create 11.cpp

---
+++ WEEK6/11.cpp
@@ -0,0 +1,40 @@
+void printSpiral(struct node* root)
+{
+    int h = height(root);
+    int i;
+  
+    bool ltr = false;
+    for(i = 1; i <= h; i++)
+    {
+        printGivenLevel(root, i, ltr);
+  
+        ltr = !ltr;
+    }
+}
+ 
+void printGivenLevel(struct node* root, 
+                     int level, int ltr)
+{
+    if (root == NULL)
+        return;
+    if (level == 1)
+        cout << root->data << " ";
+          
+    else if (level > 1) 
+    {
+        if (ltr)
+        {
+            printGivenLevel(root->left, 
+                            level - 1, ltr);
+            printGivenLevel(root->right,
+                            level - 1, ltr);
+        }
+        else 
+        {
+            printGivenLevel(root->right, 
+                            level - 1, ltr);
+            printGivenLevel(root->left,
+                            level - 1, ltr);
+        }
+    }
+}
