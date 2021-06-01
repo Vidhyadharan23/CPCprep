acec70adca73356abed57a1c7c38e1207d7f8304
Vidhyadharan23 <79758619+Vidhyadharan23@users.noreply.github.com>
2021-06-01 13:19:20 +0530

    Create 8.cpp

---
+++ WEEK6/8.cpp
@@ -0,0 +1,15 @@
+void populateNext(node* p)
+{
+    static node *next = NULL;
+ 
+    if (p)
+    {
+        populateNext(p->right);
+
+        p->next = next;
+ 
+        next = p;
+
+        populateNext(p->left);
+    }
+}
