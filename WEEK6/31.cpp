085daf6f7cd4a126e3bd9c016a4a5d00d62de12a
Vidhyadharan23 <79758619+Vidhyadharan23@users.noreply.github.com>
2021-06-03 06:56:45 +0530

    Create 31.cpp

---
+++ WEEK6/31.cpp
@@ -0,0 +1,24 @@
+int sum(struct node *root)
+{
+    if (root == NULL)
+        return 0;
+     
+    return sum(root->left) + root->data + sum(root->right);
+}
+
+int isSumTree(struct node* node)
+{
+    int ls, rs;
+    if (node == NULL ||
+       (node->left == NULL &&
+        node->right == NULL))
+        return 1;
+  
+   ls = sum(node->left);
+   rs = sum(node->right);
+
+    if ((node->data == ls + rs) && isSumTree(node->left) && isSumTree(node->right))
+        return 1;
+ 
+   return 0;
+}
