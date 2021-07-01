60dad0b05539955af3d3556395ff4ae60d560158
Vidhyadharan23 <79758619+Vidhyadharan23@users.noreply.github.com>
2021-07-01 12:40:13 +0530

    Create 2.cpp

---
+++ WEEK10/2.cpp
@@ -0,0 +1,17 @@
+void permute(string a, int l, int r)
+{
+    // Base case
+    if (l == r)
+        cout<<a<<endl;
+    else
+    {
+        for (int i = l; i <= r; i++)
+        {
+            swap(a[l], a[i]);
+          
+            permute(a, l+1, r);
+          
+            swap(a[l], a[i]);
+        }
+    }
+}
